4f3f6ad92b130737083f3d066b277db0be42ec2d
tuanmmm <nguyenthetuan100902@gmail.com>
2022-03-11 08:27:52 +0700

    buoi 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const productRouter = require('./routes/product');
+// const express = require("express");
+import express from 'express';
+import productRouter from './routes/product';
+// const productRouter = require('./routes/product');
 const app = express();
 // middleware
 app.use(express.json());
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,18 +1,27 @@
 
-const { Router } = require('express');
-const { checkAuth } = require('../middleware/checkAuth');
+import { Router } from 'express';
+import { checkAuth } from '../middleware/checkAuth';
 
 const router = Router();
-
-router.get("/products", checkAuth, (req, res) => {
-    const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
-  res.json(products);
+ const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
+router.get("/product", checkAuth, (req, res) => { // get all
+    res.json(products);
 });
-
-router.post('/products', checkAuth, (req, res) => {
-    const products = [{id: 1,name: "Product A",},{id: 2,name: "Product B",},];
+router.get("/product/:id",(req , res) => { // get a product
+    const result = products.find(item => item.id === +req.params.id);
+    res.json(result);
+});
+router.post('/product', checkAuth, (req, res) => {
     products.push(req.body);
     res.json(products);
 });
+router.delete("/product/:id", (req, res) => { // delete product
+  const newProducts = products.filter(item => item.id !== +req.params.id);
+  res.json(newProducts);
+});
+router.put("/product/:id", (req, res) => { // update product
+  const newProducts = products.map(item => item.id === +req.params.id ? req.body : item)
+  res.json(newProducts);
+})
 
-module.exports = router;
+export default router;
